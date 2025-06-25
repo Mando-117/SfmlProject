191738af41b6922d1a289c7529a2101b7e9c68eb
Mando-117 <lozanoarmando00@gmail.com>
2025-06-25 15:01:08 -0700

    added focus and unfocused states that change title

--- main.cpp
+++ main.cpp
@@ -2,15 +2,41 @@
 
 int main()
 {
-    sf::Window window(sf::VideoMode({800, 600}), "SFML Window");
+    sf::Window window(sf::VideoMode({800,800}), "SFML Window");
+
+    // change the position of the window (relatively to the desktop)
+    window.setPosition({1000, 400});
+
+    // change window size
+    //window.setSize({800, 800});
+
+    // get the size of the window
+    sf::Vector2u size = window.getSize();
+    auto [width, height] = size;
+
+    // change the title
+    window.setTitle("First Game");
+
+    // see if window has focus
+    //bool focus = window.hasFocus();
+
 
     while (window.isOpen())
     {
+        bool focus = window.hasFocus();
+
+        if (focus)
+            window.setTitle("This is a focused window");
+        else
+            window.setTitle("This is a unfocused window");
+
         while (const std::optional event = window.pollEvent())
         {
+            bool focus = window.hasFocus();
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
     }
 
+    window.setFramerateLimit(60);
 }
