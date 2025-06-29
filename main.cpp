76de08890da86aee7331c115e28dd659e15f270e
Mando-117 <lozanoarmando00@gmail.com>
2025-06-29 15:10:06 -0700

    centered text to shape and added bounce effect when shape hits the window, adding config file and fonts as examples

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <SFML/Graphics.hpp>
 #include <fstream>
 #include <string>
-#include "EntityShapes.h"
 
 struct codedShape
 {
@@ -19,8 +18,10 @@ struct codedShape
 int main()
 {
     std::fstream fileIn;
-    std::string font;
     std::string fileWord;
+
+    sf::Font font;
+    float fontSize , fontRVal, fontGVal, fontBVal;
 
     unsigned int windowX = 0, windowY= 0;
 
@@ -29,19 +30,56 @@ int main()
     std::vector<sf::CircleShape> circleShapes;
 
 
-    fileIn.open("config.txt");
+    fileIn.open("../config.txt");
     if (!fileIn.is_open())
     {
         std::cerr << "Error opening file" << std::endl;
     }
-    while (fileIn >> fileWord) {
+    while (fileIn >> fileWord)
+    {
         if (fileWord == "Window")
         {
             fileIn >> windowX >> windowY;
         }
         if (fileWord == "Font")
         {
-            font = fileWord;
+            std::string fontLoc, fontFormat;
+            fileIn >> fontLoc;;
+            fontFormat = "../"+ fontLoc;
+
+            std::cout<<fontFormat;
+            if (!font.openFromFile(fontFormat))
+            {
+                std::cout<<"Error opening file"<<std::endl;
+                return -1;
+            }
+
+            fileIn >> fontSize >> fontRVal >> fontGVal >> fontBVal;
+
+        }
+        if (fileWord == "Circle")
+        {
+            std::string shapeType = fileWord;
+            std::string cName;
+
+            float cPosX, cPosY, cVelX, cVelY, cRVal, cGVal, cBVal, cRadius;
+
+            fileIn >> cName >> cPosX >> cPosY >> cVelX >> cVelY >> cRVal >> cGVal >> cBVal >> cRadius;
+
+            sf::Text circleText(font);
+
+            sf::CircleShape circle(cRadius);
+            circle.setPosition(sf::Vector2f(cPosX,cPosY));
+            circle.setFillColor(sf::Color(cRVal,cGVal,cBVal));
+            circleShapes.push_back(circle);
+
+            struct codedShape newCircle;
+            newCircle.shapeType = shapeType;
+            newCircle.circle = circle;
+            newCircle.shapeName = cName;
+            newCircle.velocityX = cVelX;
+            newCircle.velocityY = cVelY;
+            allShapes.push_back(newCircle);
         }
         if (fileWord == "Rectangle")
         {
@@ -65,33 +103,11 @@ int main()
             allShapes.push_back(newRectangle);
 
         }
-        if (fileWord == "Circle")
-        {
-            std::string shapeType = fileWord;
-            std::string cName;
-            float cPosX, cPosY, cVelX, cVelY, cRVal, cGVal, cBVal, cRadius;
-
-            fileIn >> cName >> cPosX >> cPosY >> cVelX >> cVelY >> cRVal >> cGVal >> cBVal >> cRadius;
-
-
-            sf::CircleShape circle(cRadius);
-            circle.setPosition(sf::Vector2f(cPosX,cPosY));
-            circle.setFillColor(sf::Color(cRVal,cGVal,cBVal));
-            circleShapes.push_back(circle);
-
-            struct codedShape newCircle;
-            newCircle.shapeType = shapeType;
-            newCircle.circle = circle;
-            newCircle.shapeName = cName;
-            newCircle.velocityX = cVelX;
-            newCircle.velocityY = cVelY;
-            allShapes.push_back(newCircle);
-        }
     }
 
 
 
-        sf::RenderWindow window(sf::VideoMode({windowX, windowY}), "First Game");
+        sf::RenderWindow window(sf::VideoMode({windowX, windowY}), "First Sfml Project");
 
         window.setFramerateLimit(60);
 
@@ -110,24 +126,71 @@ int main()
             window.clear(sf::Color::Black);
 
 
-            // draw everything here...
+            // draw everything here
             for (auto& shapeToDraw : allShapes)
             {
                 if (shapeToDraw.shapeType== "Rectangle")
                 {
+                    sf::Text rectText(font);
+                    rectText.setCharacterSize(fontSize);
+                    rectText.setString(shapeToDraw.shapeName);
+                    rectText.setFillColor(sf::Color(fontRVal,fontGVal,fontBVal));
+
+                    sf::FloatRect textBounds = rectText.getLocalBounds();
+                    rectText.setOrigin(sf::Vector2f(textBounds.position.x + textBounds.size.x / 2.f,
+                                                        textBounds.position.y + textBounds.size.y / 2.f));
+                    rectText.setPosition(sf::Vector2f(shapeToDraw.rect.getPosition().x + shapeToDraw.rect.getSize().x/2.f,
+                                                        shapeToDraw.rect.getPosition().y + shapeToDraw.rect.getSize().y/2.f));
+
                     window.draw(shapeToDraw.rect);
-                    //shapeToDraw.rect.setPosition(sf::Vector2f(shapeToDraw.rect.getPosition().x + shapeToDraw.getVelocityX(), shapeToDraw.rect.getPosition().y + shapeToDraw.getVelocityY()));
+                    window.draw(rectText);
+
+                    sf::FloatRect bounceBounds = shapeToDraw.rect.getGlobalBounds();
+
+                    if (bounceBounds.position.y + bounceBounds.size.y > windowY || bounceBounds.position.y < 0)
+                    {
+                        shapeToDraw.velocityY *= -1;
+                    }
+                    if (bounceBounds.position.x + bounceBounds.size.x > windowX || bounceBounds.position.x < 0)
+                    {
+                        shapeToDraw.velocityX *= -1;
+                    }
+
                     shapeToDraw.rect.move(sf::Vector2f(shapeToDraw.getVelocityX(), shapeToDraw.getVelocityY()));
                 }
-                else if (shapeToDraw.shapeType == "Circle")
-                {
-                    window.draw(shapeToDraw.circle);
-                    //shapeToDraw.circle.setPosition(sf::Vector2f(shapeToDraw.circle.getPosition().x + shapeToDraw.getVelocityX(), shapeToDraw.circle.getPosition().y + shapeToDraw.getVelocityY()));
-                }
+                else if (shapeToDraw.shapeType == "Circle") {
+                    sf::Text circleText(font);
+                    circleText.setCharacterSize(fontSize);
+                    circleText.setString(shapeToDraw.shapeName);
+                    circleText.setFillColor(sf::Color(fontRVal,fontGVal,fontBVal));
 
+                    sf::FloatRect textBounds = circleText.getLocalBounds();
+                    circleText.setOrigin(sf::Vector2f(textBounds.position.x + textBounds.size.x / 2.f,
+                                                        textBounds.position.y + textBounds.size.y/2.f));
+
+                    circleText.setPosition(sf::Vector2f(shapeToDraw.circle.getPosition().x + shapeToDraw.circle.getRadius(),
+                                            shapeToDraw.circle.getPosition().y + shapeToDraw.circle.getRadius()));
+
+                    window.draw(shapeToDraw.circle);
+                    window.draw(circleText);
+
+                    sf::FloatRect bounceBounds = shapeToDraw.circle.getGlobalBounds();
+
+
+                    if (bounceBounds.position.y + bounceBounds.size.y > windowY|| bounceBounds.position.y < 0)
+                    {
+                        shapeToDraw.velocityY *= -1;
+                    }
+                    if (bounceBounds.position.x + bounceBounds.size.x > windowX || bounceBounds.position.x < 0)
+                    {
+                        shapeToDraw.velocityX *= -1;
+                    }
+
+                    shapeToDraw.circle.move(sf::Vector2f(shapeToDraw.getVelocityX(), shapeToDraw.getVelocityY()));
+
+                }
             }
 
-            // end the current frame
             window.display();
         }
 
